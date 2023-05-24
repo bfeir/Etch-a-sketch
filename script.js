8df6df4849fd61bda9368fb38f592363a4b7cb0b
Benjamin Feireisen <benjamin.feireisen@gmail.com>
2023-05-24 22:58:38 +0200

    Create user input and reset grid

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.container');
 
-for(i=0; i<16; i++) {
+function createGrid(userNumber) {
+for(i=0; i<userNumber; i++) {
         const row = document.createElement('div');
         row.id = "row" + i;
         row.className = "row";
@@ -8,21 +9,28 @@ for(i=0; i<16; i++) {
 
         const rowNumber = document.getElementById("row" + i);
 
-    for(j=0; j<16; j++) {
+    for(j=0; j<userNumber; j++) {
         
         const gridCell = document.createElement('div');
         gridCell.classList.add('gridCell');
         rowNumber.appendChild(gridCell);
+        }
+
     }
-
-}
-
-const cellSelection = document.querySelectorAll('.gridCell');
-cellSelection.forEach( function(element) {
+    const cellSelection = document.querySelectorAll('.gridCell');
+    cellSelection.forEach( function(element) {
     element.addEventListener('mouseenter', () => {
-        element.style.backgroundColor = 'black';
-    } );
-    element.addEventListener('mouseleave', () => {
-        element.style.backgroundColor = 'white';
+    element.style.backgroundColor = 'black';
     });
 });
+}
+
+function chooseGridSize() {
+    document.querySelectorAll('.row').forEach(e => e.remove());
+    
+    let userNumber = Number(window.prompt("Type a number", ""));
+    if (userNumber >= 100) userNumber = 100;
+    createGrid(userNumber);
+    }
+
+createGrid(16);
